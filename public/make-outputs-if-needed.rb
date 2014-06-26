75d9a1dbba1a6c072fcddc20c01b243b6c3b284d
Tom Counsell <tom@counsell.org>
2014-06-26 19:43:18 +0100

    Now sets directory before trying to update book versions

--- public/make-outputs-if-needed.rb
+++ public/make-outputs-if-needed.rb
@@ -1,6 +1,7 @@
 trigger = File.join(File.dirname(__FILE__),"UPDATE")
 exit unless File.exist?(trigger)
 File.delete(trigger)
+Dir.chdir(File.join(File.dirname(__FILE__),'..'))
 puts `git pull`
 puts `ruby #{File.dirname(__FILE__)}/make-pdf.rb`
 puts `ruby #{File.dirname(__FILE__)}/make-epub.rb`
