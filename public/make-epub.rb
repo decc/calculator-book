b48d60e971880f6777f07490b549d6c0e0bb7a4f
Tom Counsell <tom@counsell.org>
2014-06-26 19:11:30 +0100

    Updated folder name in scripts

--- public/make-epub.rb
+++ public/make-epub.rb
@@ -2,4 +2,4 @@ Dir.chdir(File.join(File.dirname(__FILE__),'..'))
 contents = IO.readlines("contents.txt").map(&:strip)
 contents.delete_if { |line| line =~ /^#/ }
 
-puts `pandoc --toc -o outputs/calculator-book.epub #{contents.join(" ")}`
+puts `pandoc --toc -o public/calculator-book.epub #{contents.join(" ")}`
--- public/make-website.rb
+++ public/make-website.rb
@@ -1,5 +1,5 @@
 Dir.chdir(File.join(File.dirname(__FILE__),'..'))
 contents = IO.readlines("contents.txt").map(&:strip)
 contents.delete_if { |line| line =~ /^#/ }
-puts `pandoc -s -S -t docbook -o outputs/calculator-book.db #{contents.join(" ")}`
-puts `xmlto xhtml --skip-validation -m outputs/config.xml outputs/calculator-book.db -o outputs/calculator-book/`
+puts `pandoc -s -S -t docbook -o public/calculator-book.db #{contents.join(" ")}`
+puts `xmlto xhtml --skip-validation -m public/config.xml public/calculator-book.db -o public/calculator-book/`
