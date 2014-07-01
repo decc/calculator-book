9603083e58d3e07d8842344dc4f3dd871b35669b
James Geddes <james@jamesgeddes.org>
2014-07-01 09:24:31 +0100

    Rebuilding the book now recompiles any .dot files in figures/

--- public/make-outputs-if-needed.rb
+++ public/make-outputs-if-needed.rb
@@ -4,6 +4,7 @@ exit unless File.exist?(trigger)
 File.delete(trigger)
 ## Convert any '.dot' files in the figures/ directory to pdfs
 Dir.chdir(File.join(root, '../figures'))
+puts `make clean`
 puts `make`
 ## Make multiple output versions of the book
 Dir.chdir(File.join(root,'..'))
