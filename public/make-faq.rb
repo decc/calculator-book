dc00712b4ab1b2f2a98e2f505a02299ce94a1c61
Tom Counsell <tom@counsell.org>
2014-09-09 14:14:33 +0100

    Updated FAQ

--- public/make-faq.rb
+++ public/make-faq.rb
@@ -1,16 +1,16 @@
 # encoding: utf-8
-raw = IO.readlines(File.join(File.dirname(__FILE__), '..', 'faq.tsv'))
+require 'csv'
+file = File.join(File.dirname(__FILE__), '..', 'faq.csv')
 
 categorised = Hash.new { |hash, key| hash[key] = [] }
 
-raw.each do |line|
-  cells = line.split("\t")
+CSV.foreach(file) do |cells|
   category = cells[0]
   id = cells[1]
   question = cells[2]
   answer = cells[3]
   next if question.strip.empty?
-  categorised[category.strip].push [question, answer]
+  categorised[category.strip].push [id, question, answer]
 end
 
 File.open(File.join(File.dirname(__FILE__), '..', 'faq.md'), 'w') do |f|
@@ -20,11 +20,10 @@ File.open(File.join(File.dirname(__FILE__), '..', 'faq.md'), 'w') do |f|
     f.puts "## #{category}"
     f.puts
     faqs.each do |faq|
-      f.puts "#{faq.first}"
+      f.puts "#### FAQ#{faq[0]}) #{faq[1]}"
       f.puts
-      f.puts "~    #{faq.last.split("\n").join("\n    ")}"    
+      f.puts faq[2].gsub("\\n","\n\n")
       f.puts
     end
   end
-
 end
