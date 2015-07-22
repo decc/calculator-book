95d3fc84b2024d18609ea1ad4f24c0443ee51ce3
Tom Counsell <tamc@greenonblack.com>
2015-07-22 11:50:29 +0100

    Updated server to allow get for updates as well as post

--- server.rb
+++ server.rb
@@ -3,9 +3,22 @@ require 'json'
 
 figures_folder = File.join(File.dirname(File.expand_path(__FILE__)), 'figures')
 
-post '/update' do
+def trigger_update
   File.open(File.expand_path('UPDATE', settings.public_folder),'w') { |f| f.puts Time.now } 
   "ok"
+end
+
+post '/update' do
+  trigger_update
+end
+
+get '/update' do
+  trigger_update
+end
+
+# Used to check server is working!
+get '/timenow' do
+  Time.now.to_s
 end
 
 get '/calculator-book/figures/*.*' do |path, ext|
